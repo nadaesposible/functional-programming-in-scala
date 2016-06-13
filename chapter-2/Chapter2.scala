a6e19c939ca70e55a78f4cea1c5ede95d0330d1a
Pedro Galan <pedro.galan@piksel.com>
2016-06-13 13:36:49 +0100

    Add final examples of chapter 2.

--- chapter-2/Chapter2.scala
+++ chapter-2/Chapter2.scala
@@ -28,4 +28,12 @@ object Chapter2 {
   def curry[A,B,C](f: (A, B) => C): A => (B => C) = {
     a: A => b: B => f(a, b)
   }
+
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = {
+    (a, b) => f(a)(b)
+  }
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    a => f(g(a))
+  }
 }
